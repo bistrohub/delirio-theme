0085bf50c5b6154339542a0a47e4687894c62898
Mario Campa <mcampa@gmail.com>
2017-04-16 22:48:41 -0700

    jquery.unveil.js

---
+++ assets/js/jquery.unveil.js
@@ -0,0 +1,52 @@
+/**
+ * jQuery Unveil
+ * A very lightweight jQuery plugin to lazy load images
+ * http://luis-almeida.github.com/unveil
+ *
+ * Licensed under the MIT license.
+ * Copyright 2013 Luís Almeida
+ * https://github.com/luis-almeida
+ */
+
+;(function($) {
+    $.fn.unveil = function(threshold, callback) {
+        var $w = $(window);
+        var th = threshold || 0;
+        var retina = window.devicePixelRatio > 1;
+        var attrib = retina ? "data-src-retina" : "data-src";
+        var images = this;
+        var loaded;
+
+        this.one("unveil", function() {
+            var source = this.getAttribute(attrib);
+            source = source || this.getAttribute("data-src");
+            if (source) {
+                this.setAttribute("src", source);
+                if (typeof callback === "function") callback.call(this);
+            }
+        });
+
+        function unveil() {
+            var inview = images.filter(function() {
+                var $e = $(this);
+                if ($e.is(":hidden")) return;
+
+                var wt = $w.scrollTop(),
+                    wb = wt + $w.height(),
+                    et = $e.offset().top,
+                    eb = et + $e.height();
+
+                return eb >= wt - th && et <= wb + th;
+            });
+
+            loaded = inview.trigger("unveil");
+            images = images.not(loaded);
+        }
+
+        $w.on("scroll.unveil resize.unveil lookup.unveil", unveil);
+
+        unveil();
+
+        return this;
+    };
+})(window.jQuery);
